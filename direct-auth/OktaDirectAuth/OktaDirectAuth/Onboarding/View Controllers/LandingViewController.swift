525a1a3a4b2734e1313e356114b622a075061f0d
Mike Nachbaur <74688448+mikenachbaur-okta@users.noreply.github.com>
2021-05-11 09:42:28 -0700

    Add registration and password enrolment (#92)

--- direct-auth/OktaDirectAuth/OktaDirectAuth/Onboarding/View Controllers/LandingViewController.swift
+++ direct-auth/OktaDirectAuth/OktaDirectAuth/Onboarding/View Controllers/LandingViewController.swift
@@ -16,7 +16,10 @@ import OktaIdxAuth
 
 protocol SigninController {
     var auth: OktaIdxAuth? { get set }
+    var response: OktaIdxAuth.Response? { get set }
+    
     func show(error: Error)
+    func showController(for response: OktaIdxAuth.Response, with storyboardId: String)
 }
 
 extension SigninController where Self: UIViewController {
@@ -31,6 +34,20 @@ extension SigninController where Self: UIViewController {
         }))
         present(alert, animated: true)
     }
+    
+    func showController(for response: OktaIdxAuth.Response, with storyboardId: String) {
+        guard let navigationController = navigationController,
+              var viewController = storyboard?.instantiateViewController(identifier: storyboardId) as? UIViewController & SigninController
+        else {
+            show(error: OnboardingError.missingViewController)
+            return
+        }
+        
+        viewController.auth = auth
+        viewController.response = response
+        
+        navigationController.setViewControllers([viewController], animated: true)
+    }
 }
 
 class LandingViewController: UIViewController, SigninController {
@@ -39,6 +56,7 @@ class LandingViewController: UIViewController, SigninController {
     @IBOutlet weak private(set) var backgroundImageView: UIImageView!
     @IBOutlet weak private(set) var footerView: UIView!
     var auth: OktaIdxAuth?
+    var response: OktaIdxAuth.Response?
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
